1068e6e1d84844e59bd42f43402d4bd836031b28
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-03-15 15:02:10 -0300

    Fix eslint shadow rules on api

--- packages/api/.eslintrc.js
+++ packages/api/.eslintrc.js
@@ -19,6 +19,9 @@ module.exports = {
     // typescript conflicting rules
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+
     'import/extensions': ['error', 'ignorePackages', { ts: 'never' }],
   },
   settings: {
