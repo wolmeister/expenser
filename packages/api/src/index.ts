00cee52ecffa8f360493524a45e7dec804f63582
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-03-15 15:10:09 -0300

    Setup objection

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,11 +1,11 @@
-// import { Model } from 'objection';
+import { Model } from 'objection';
 
 import debug from './debug';
 import { knex } from './knex';
 import { app } from './app';
 
 // Setup knex
-// Model.knex(knex);
+Model.knex(knex);
 
 // Init express
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
