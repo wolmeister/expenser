07aab6f80fc22b5f36b5134157010a34cccbc41f
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-03-15 11:11:07 -0300

    Setup api project with express

---
+++ packages/api/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send({ hello: 'world' });
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
