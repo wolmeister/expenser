018e4e75d1552cf7a135d3720f9f49597af2f223
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-03-15 23:12:00 -0300

    [WEB] Setup eslint

---
+++ packages/web/.eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'react-hooks', 'react', 'prettier'],
+  rules: {
+    // Base rules
+    'class-methods-use-this': 'off',
+
+    // Prettier
+    'prettier/prettier': 'error',
+
+    // Import
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['error', 'ignorePackages', { ts: 'never', tsx: 'never' }],
+
+    // Typescript rules
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+    // Typescript conflicting rules
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+
+    // A11y
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+    'jsx-a11y/label-has-for': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
+        },
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      // this loads <rootdir>/tsconfig.json to eslint
+      typescript: {},
+    },
+  },
+};
