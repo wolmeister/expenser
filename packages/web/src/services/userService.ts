f3405d08aea38dd1d06826aa745a6e83b9390683
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-03-16 19:51:41 -0300

    [WEB] Remove old services

--- packages/web/src/services/userService.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../models/user';
-import { http } from './http';
-
-export async function getAuthenticatedUser(): Promise<User> {
-  return http.get('/api/users/me').json();
-}
