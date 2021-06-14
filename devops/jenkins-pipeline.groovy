98a41f5bbd0c7402769b98962765232bd574b2d6
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-06-14 20:56:20 -0300

    [DEVOPS] Add build and publish jenkins pipeline

---
+++ devops/jenkins-pipeline.groovy
@@ -0,0 +1,52 @@
+node {
+  def DOCKER_REGISTRY = 'http://177.44.248.70:5000'
+
+  stage('Checkout repository') {
+    git branch: 'main', url: 'https://github.com/wolmeister/expenser.git'
+  }
+
+  stage('Install dependencies') {
+    // sh 'pnpm install'
+  }
+
+  stage('Build and publish') {
+    parallel([
+      'API': {
+        def image;
+
+        stage('Build') {
+          sh 'echo Build'
+        }
+        stage('Test') {
+          sh 'echo test'
+        }
+        stage('Bake image') {
+          image = docker.build("expenser-api:${env.BUILD_TAG}", "./packages/api")
+        }
+        stage('Publish image') {
+          docker.withRegistry(DOCKER_REGISTRY) {
+            image.push 'latest'
+          }
+        }
+      },
+      'WEB': {
+        def image;
+        
+        stage('Build') {
+          sh 'echo Build'
+        }
+        stage('Test') {
+          sh 'echo test'
+        }
+        stage('Bake image') {
+          image = docker.build("expenser-web:${env.BUILD_TAG}", "./packages/web")
+        }
+        stage('Publish image') {
+          docker.withRegistry(DOCKER_REGISTRY) {
+            image.push 'latest'
+          }
+        }
+      }
+    ])
+  }
+}
