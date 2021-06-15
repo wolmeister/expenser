8855fb02458135b530faed04ae3a9030c42adf00
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-06-15 00:52:06 -0300

    [API] Change title to test deploy

--- packages/web/src/pages/Home/index.tsx
+++ packages/web/src/pages/Home/index.tsx
@@ -92,7 +92,7 @@ export function Home() {
         <Zap fill="black" />
         <Spacer x={0.5} />
         <Text h4 style={{ margin: 0 }}>
-          Expenser - Test
+          Expenser - Automatic Deploy
         </Text>
         <Text small style={{ cursor: 'pointer', marginLeft: 'auto' }} onClick={signOut}>
           Logout
